de4e18b4617b2ab4106b680a67a9652f4102f270
Christopher McKee <chris.mckee.13@gmail.com>
2020-03-11 21:01:51 -0700

    removed filler react files

---
+++ src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Card(props) {
+  return (
+    <div
+      className="card"
+      style={{}}
+    >
+    </div>
+  );
+}
+
+export default Card;
