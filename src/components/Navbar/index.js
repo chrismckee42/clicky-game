665314d7410e0a127c96c07e838c3ee7f2399e30
Christopher McKee <chris.mckee.13@gmail.com>
2020-03-13 14:41:59 -0700

    fixes navbar layout

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,23 @@
 import React from "react";
-import "./style.css";
+//import "./style.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div>
+      
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
         
+        <div className="navbar-header navbar-left text-white">         
+          Clicky Game
+        </div>
+        <div className="navbar-center text-white">
+          Click an image to begin!
+        </div>
+        <div className="nav navbar-nav navbar-right text-white">Score: 0 | Top Score: 0 </div>
       </div>
     </nav>
+
   );
 }
 
